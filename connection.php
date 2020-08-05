64962d76a01a252d776baa48c1db0c2e5637b7d4
DESKTOP-SPNOEQI <jjescuderv@eafit.edu.co>
2020-08-05 15:47:46 -0500

    Atributos de PDO

--- connection.php
+++ connection.php
@@ -6,6 +6,7 @@
     
     try {
       $conn = new PDO("mysql:host=$servername; dbname=$db", $username, $password);
+      $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     } catch(PDOException $e) {
       echo "Connection failed: " . $e->getMessage();
     }
